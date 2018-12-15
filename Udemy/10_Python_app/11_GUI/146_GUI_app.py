7f7fe28079e178da9dd83594ac1c7f8628b20611
Rafal <rafal.datascience@gmail.com>
2018-12-15 21:01:02 +0100

    End of chapter 11 GUI Tkinter

--- Udemy/10_Python_app/11_GUI/146_GUI_app.py
+++ Udemy/10_Python_app/11_GUI/146_GUI_app.py
@@ -2,14 +2,35 @@ from tkinter import *
 
 window = Tk()
 
-b1=Button(window, text = 'Execute')
-b1.grid(row =0, column =0)
+def km_to_miles():
+    grams = round((int(e1_value.get()) * 1000),2)
+    t1.delete("1.0", END)
+    t1.insert(END, grams)
+    pounds = round((int(e1_value.get()) * 2.20462),2)
+    t2.delete("1.0", END)
+    t2.insert(END, pounds)
+    ounces = round((int(e1_value.get()) * 35.274), 2)
+    t3.delete("1.0", END)
+    t3.insert(END, ounces)
 
-e1 = Entry(window)
+l1 = Label(window, text = 'kg', )
+l1.pack(side='bottom')
+l1.grid(row=0, column=0)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value, width=10)
 e1.grid(row=0, column=1)
 
-t1 = Text(window, height=1, width=20)
-t1.grid(row=0, column=2)
+b1=Button(window, text = 'Convert', command=km_to_miles)
+b1.grid(row =0, column =2)
 
+t1 = Text(window, height=1, width=7)
+t1.grid(row=1, column=0)
+
+t2 = Text(window, height=1, width=7)
+t2.grid(row=1, column=1)
+
+t3 = Text(window, height=1, width=7)
+t3.grid(row=1, column=2)
 
 window.mainloop()

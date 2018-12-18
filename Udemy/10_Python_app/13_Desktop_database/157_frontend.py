7e318be4283441019180a8e648b5bd254a473109
Rav212mm <rafal.sabak@gmail.com>
2018-12-18 19:28:21 +0100

    Update of lecture 161

--- Udemy/10_Python_app/13_Desktop_database/157_frontend.py
+++ Udemy/10_Python_app/13_Desktop_database/157_frontend.py
@@ -1,11 +1,32 @@
 from tkinter import *
 import backend
 
+
+def view_command():
+    list1.delete(0, END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+
+def search_command():
+    list1.delete(0, END)
+    for row in backend.search(e1_value.get(), e2_value.get(), e3_value.get(), e4_value.get()):
+        list1.insert(END, row)
+
+
+def insert_command():
+    backend.insert(e1_value.get(), e2_value.get(),
+                   e3_value.get(), e4_value.get())
+    list1.delete(0, END)
+    row = e1_value.get(), e2_value.get(), e3_value.get(), e4_value.get()
+    list1.insert(END, row)
+
+def get_selected_row(event):
+    index = list1.curselection()
+    print(index)
+
 window = Tk()
 window.title("Library master")
-
-def dzialaj():
-    print('Przycisk działa !')
 
 l1 = Label(window, text='Title', width=7)
 l1.grid(row=0, column=0)
@@ -19,7 +40,7 @@ l2 = Label(window, text='Author', width=7)
 l2.grid(row=0, column=2)
 
 e2_value = StringVar()
-e2 = Entry(window, textvariable=e2_value, width=15)
+e2 = Entry(window, textvariable=e2_value, width=20)
 e2.grid(row=0, column=3)
 
 l3 = Label(window, text='Year', width=7)
@@ -36,7 +57,7 @@ e4_value = StringVar()
 e4 = Entry(window, textvariable=e4_value, width=15)
 e4.grid(row=1, column=3)
 
-list1 = Listbox(window, height=6, width=35)
+list1 = Listbox(window, height=6, width=45)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1=Scrollbar(window)
@@ -45,20 +66,23 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1 = Button(window, text='View all', width =12, command = dzialaj)
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1 = Button(window, text='View all', width =12, command = view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text='Search entry', width=12, command=print('View all working'))
+b2 = Button(window, text='Search entry', width=12, command = search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text='Add entry', width=12, command=print('View all working'))
+b3 = Button(window, text='Add entry', width=12, command=insert_command)
 b3.grid(row=4, column=3)
 
 b4 = Button(window, text='Update selected', width=12, command=print('View all working'))
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text='Delete selected', width=12, command=print('View all working'))
+b5 = Button(window, text='Delete selected', width=12) ##, command=delete_command
 b5.grid(row=6, column=3)
+
 
 b6 = Button(window, text='Close', width=12, command=print('View all working'))
 b6.grid(row=7, column=3)
--- Udemy/10_Python_app/13_Desktop_database/backend.py
+++ Udemy/10_Python_app/13_Desktop_database/backend.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 def connect():
-    conn = sqlite3.connect('books.db')
-    cur=conn.cursor()
+    conn = sqlite3.connect('./books.db')
+    cur = conn.cursor()
     cur.execute(
         "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     conn.commit()
@@ -10,16 +10,16 @@ def connect():
 
 def insert(title, author, year, isbn):
     conn = sqlite3.connect('books.db')
-    cur=conn.cursor()
+    cur = conn.cursor()
     cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
     conn.commit()
     conn.close()
 
 def view():
-    conn=sqlite3.connect('books.db')
-    cur=conn.cursor()
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
     cur.execute('SELECT * FROM book')
-    rows=cur.fetchall()
+    rows = cur.fetchall()
     conn.close()
     return rows
 
@@ -33,7 +33,7 @@ def search(title='', author='', year='', isbn=''):
 
 def delete(id):
     conn = sqlite3.connect('books.db')
-    cur=conn.cursor()
+    cur = conn.cursor()
     cur.execute("DELETE FROM book WHERE id=?", (id,))
     conn.commit()
     conn.close()
